fad191ac35e5cbbbe7f36e40bea54cba797cfa8f
oogwu <oogwu@tencent.com>
2023-09-06 13:45:07 +0800

    Fix search.js

--- search.js
+++ search.js
@@ -13,36 +13,41 @@ if (os.platform() === 'darwin') {
 }
 
 module.exports = (pluginContext) => {
-  return (query, env) => {
-    if (query.length === 0) return Promise.resolve([])
-    const variables = env || {}
-    const bookmarkFile = (variables['file'] || file).replace(/^~/, os.homedir())
-    return new Promise((resolve, reject) => {
-      fs.readFile(bookmarkFile, (err, data) => {
-        const rawData = JSON.parse(data)
-        // Find
-        const bookmarks = []
-        traverse(rawData).forEach((item) => {
-          const isBookmark = item['type'] === 'url'
-          if (isBookmark) {
-            const bookmark = {
-              id: query + item['url'],
-              title: item['name'],
-              subtitle: item['url'],
-              value: item['url'],
+  return {
+    respondsTo: (query, env = {}) => {
+      return true
+    },
+    search: (query, env) => {
+      if (query.length === 0) return Promise.resolve([])
+      const variables = env || {}
+      const bookmarkFile = (variables['file'] || file).replace(/^~/, os.homedir())
+      return new Promise((resolve, reject) => {
+        fs.readFile(bookmarkFile, (err, data) => {
+          const rawData = JSON.parse(data)
+          // Find
+          const bookmarks = []
+          traverse(rawData).forEach((item) => {
+            const isBookmark = item['type'] === 'url'
+            if (isBookmark) {
+              const bookmark = {
+                id: query + item['url'],
+                title: item['name'],
+                subtitle: item['url'],
+                value: item['url'],
+              }
+              bookmarks.push(bookmark)
             }
-            bookmarks.push(bookmark)
-          }
-        })
+          })
 
-        // Filter
-        const accessor = (bookmark) => bookmark.title + bookmark.subtitle
-        const filteredBookmarks = fuzzyfind(query, bookmarks, {
-          accessor,
-          precision: 0.5,
-        }).slice(0, 50)
-        resolve(filteredBookmarks)
+          // Filter
+          const accessor = (bookmark) => bookmark.title + bookmark.subtitle
+          const filteredBookmarks = fuzzyfind(query, bookmarks, {
+            accessor,
+            precision: 0.5,
+          }).slice(0, 50)
+          resolve(filteredBookmarks)
+        })
       })
-    })
+    }
   }
 }
