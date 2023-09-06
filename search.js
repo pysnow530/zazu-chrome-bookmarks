5b6d1eea8f36a60f25c6eaeb283f6cc0b9d4c828
oogwu <oogwu@tencent.com>
2023-09-06 14:02:15 +0800

    Remove fuzzyfind for no prefix

--- search.js
+++ search.js
@@ -2,52 +2,51 @@
 const fs = require('fs')
 const os = require('os')
 const traverse = require('traverse')
-const fuzzyfind = require('fuzzyfind')
 
 let file = ''
 
 if (os.platform() === 'darwin') {
-  file = '~/Library/Application Support/Google/Chrome/Default/Bookmarks'
+    file = '~/Library/Application Support/Google/Chrome/Default/Bookmarks'
 } else if (os.platform() === 'linux') {
-  file = '~/.config/google-chrome/Default/Bookmarks'
+    file = '~/.config/google-chrome/Default/Bookmarks'
 }
 
 module.exports = (pluginContext) => {
-  return {
-    respondsTo: (query, env = {}) => {
-      return true
-    },
-    search: (query, env) => {
-      if (query.length === 0) return Promise.resolve([])
-      const variables = env || {}
-      const bookmarkFile = (variables['file'] || file).replace(/^~/, os.homedir())
-      return new Promise((resolve, reject) => {
-        fs.readFile(bookmarkFile, (err, data) => {
-          const rawData = JSON.parse(data)
-          // Find
-          const bookmarks = []
-          traverse(rawData).forEach((item) => {
-            const isBookmark = item['type'] === 'url'
-            if (isBookmark) {
-              const bookmark = {
-                id: query + item['url'],
-                title: item['name'],
-                subtitle: item['url'],
-                value: item['url'],
-              }
-              bookmarks.push(bookmark)
-            }
-          })
+    return {
+        respondsTo: (query, env = {}) => {
+            return true
+        },
+        search: (query, env) => {
+            if (query.length === 0) return Promise.resolve([])
+            const variables = env || {}
+            const bookmarkFile = (variables['file'] || file).replace(/^~/, os.homedir())
+            return new Promise((resolve, reject) => {
+                fs.readFile(bookmarkFile, (err, data) => {
+                    const rawData = JSON.parse(data)
+                    // Find
+                    const bookmarks = []
+                    traverse(rawData).forEach((item) => {
+                        const isBookmark = item['type'] === 'url'
+                        if (isBookmark) {
+                            const bookmark = {
+                                id: query + item['url'],
+                                title: item['name'],
+                                subtitle: item['url'],
+                                value: item['url'],
+                            }
+                            bookmarks.push(bookmark)
+                        }
+                    })
 
-          // Filter
-          const accessor = (bookmark) => bookmark.title + bookmark.subtitle
-          const filteredBookmarks = fuzzyfind(query, bookmarks, {
-            accessor,
-            precision: 0.5,
-          }).slice(0, 50)
-          resolve(filteredBookmarks)
-        })
-      })
+                    // Filter
+                    const query_words = query.split(' ')
+                    const filteredBookmarks = bookmarks.filter(bookmark => (
+                        query_words.all(
+                            word => bookmark.title.indexOf(word) !== -1
+                                || bookmark.subtitle.indexOf(word) !== -1)))
+                    resolve(filteredBookmarks)
+                })
+            })
+        }
     }
-  }
 }
